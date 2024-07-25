69320e71b00095ea537856caef1414abe2358d06
Kurtozorus <olivier.marchand26@hotmail.fr>
2024-07-25 20:21:13 +0200

    RestaurantController CRUD ajouté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function home() : Response
+    public function home(): Response
     {
-        // Call la bdd
-        // Envoyer un mail
-        // créer un rendu $this->render()
-	    return new Response('Bienvenue sur votre accueil !');
+        return new Response('Bonjour bienvenue !');
     }
 }
