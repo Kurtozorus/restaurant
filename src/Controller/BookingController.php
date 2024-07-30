9ca64766f7778bb43b4e045f78deb12d88dff95e
Kurtozorus <olivier.marchand26@hotmail.fr>
2024-07-30 16:23:23 +0200

    ajout serializer a BookingController

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,78 +4,89 @@ namespace App\Controller;
 
 use App\Entity\Booking;
 use App\Repository\BookingRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('api/booking', name: 'app_api_booking_')]
 class BookingController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private BookingRepository $repository)
+    public function __construct(
+        private EntityManagerInterface $manager, 
+        private BookingRepository $repository, 
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator)
     {
 
     }
     #[Route(name:'new', methods: 'POST')]
-        public function new():Response
+        public function new(Request $request):JsonResponse
         {
-            $booking = new Booking();
-            $booking->setGuestNumber('9');
-            $booking->setOrderDate(new \DateTime());
-            $booking->setOrderHour(new \DateTime());
-            $booking->setAllergy('Allergie teste');
+            $booking = $this->serializer->deserialize($request->getContent(), Booking::class, 'json');
             $booking->setCreatedAt(new \DateTimeImmutable());
     
             $this->manager->persist($booking);
             $this->manager->flush();
     
-            return $this->json(
-                ['message' => "Booking ressource created with {$booking->getId()} id"],
-                Response::HTTP_CREATED,
+            $responseData = $this->serializer->serialize($booking, 'json');
+            $location = $this->urlGenerator->generate(
+                'app_api_restaurant_show',
+                ['id' => $booking->getId()],
+                UrlGeneratorInterface::ABSOLUTE_URL,
             );
+
+            return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
         }
 
         #[Route('/{id}', name:'show', methods: 'GET')]
-        public function show(int $id):Response
+        public function show(int $id):JsonResponse
         {
             $booking = $this->repository->findOneBy(['id' => $id]);
-        if (!$booking) {
-            throw new \Exception("No Booking found for {$id} id");
+        if ($booking) {
+            $responseData = $this->serializer->serialize($booking, 'json');
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
         }
 
-        return $this->json(
-            ['message' => "A Booking was found : Booking Number:{$booking->getGuestNumber()} for {$booking->getId()} id"]
-            );
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
         }
 
         #[Route('/{id}', name:'edit', methods: 'PUT')]
-        public function edit(int $id):Response
+        public function edit(int $id, Request $request):JsonResponse
         {
             $booking = $this->repository->findOneBy(['id' => $id]);
-        if (!$booking) {
-            throw new \Exception("No Booking found for {$id} id");
-        }
-        $booking->setGuestNumber('12');
-        $booking->setOrderDate(new \DateTime());
-        $booking->setOrderHour(new \DateTime());
-        $booking->setAllergy('Allergy updated');
-        $booking->setUpdatedAt(new \DateTimeImmutable());
-        $this->manager->flush();
+        if ($booking) {
+            $booking = $this->serializer->deserialize(
+                $request->getContent(),
+                Booking::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $booking]
+            );
+            $booking->setUpdatedAt(new DateTimeImmutable());
 
-        return $this->redirectToRoute('app_api_booking_show', ['id' => $booking->getId()]);
+            $this->manager->flush();
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         }
-
-        #[Route('/{id}', name:'delete', methods: 'DELETE')]
-        public function delete(int $id):Response
-        {
-            $booking = $this->repository->findOneBy(['id' => $id]);
-        if (!$booking) {
-            throw new \Exception("No Booking found for {$id} id");
-        }
-        $this->manager->remove($booking);
-        $this->manager->flush();
-
-        return $this->json(['message' => 'Booking ressource deleted'], Response::HTTP_NO_CONTENT);
+        
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
         }
 
+        #[Route('/{id}', name: 'delete', methods:'DELETE')]
+    public function delete(int $id): JsonResponse
+    {
+        $booking = $this->repository->findOneBy(['id' => $id]);
+        if($booking){
+            $this->manager->remove($booking);
+            $this->manager->flush();
+            return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
 }
