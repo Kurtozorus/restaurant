dff0d20e1caa164674a510491bea75f7892c35a0
Kurtozorus <olivier.marchand26@hotmail.fr>
2024-08-05 18:04:05 +0200

    installation de nelmio api-doc-bundle et version avec ajout de fonction me() et edit() dans SecurityController+versioning

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,14 +11,17 @@ use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private SerializerInterface $serializer)
-    {
-        
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private SerializerInterface $serializer,
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {  
     }
     #[Route('/registration', name: 'registration', methods: 'POST')]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
@@ -44,5 +48,34 @@ class SecurityController extends AbstractController
             'apiToken' => $user->getApiToken(),
             'roles' => $user->getRoles()
         ],);
+    }
+    #[Route('/account/me ', name: 'me', methods: 'GET')]
+    public function me(): JsonResponse
+    {
+        $user = $this->getUser();
+
+        $responseData = $this->serializer->serialize($user, 'json');
+
+        return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+    }
+    #[Route('/account/edit', name: 'edit', methods: 'PUT')]
+    public function edit(Request $request):JsonResponse
+    {
+        $user = $this->serializer->deserialize(
+            $request->getContent(),
+            User::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE=> $this->getUser()],
+        );
+        $user->setUpdatedAt(new DateTimeImmutable());
+
+        if (isset($request->toArray()['password'])) {
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            ));
+        }
+            $this->manager->flush();
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
